c686da733a091a59328a62f9ee879f7a8ee7ec58
Benjamin Bezzina <benbezz02@gmail.com>
2023-01-27 17:42:38 +0100

    Fixing up the main for testing

--- Class.py
+++ Class.py
@@ -1,19 +1,31 @@
+import Timetables
+
 
 class Class:
 
     def __init__(self, number, year):
         self.classnumber = number
         self.year = year
-        self.numStudents = 0
+        self.maxNumStudents = 20
+        self.maxNumEmployees = 4
         self.students = []
         self.employees = []
+        self.timetable = Timetables.Timetable(number)
 
     def addStudentToClass(self, student):
+        if len(self.students) >= self.maxNumStudents:
+            print("Too many students in this class. Please create another class or add to a different class.")
+            return
+
         student.status = "Current"
 
         self.students.append(student)
 
     def addEmployeeToClass(self, employee):
+        if len(self.employees) >= self.maxNumEmployees:
+            print("Too many employees in this class. Please create another class or add to a different class.")
+            return
+
         employee.status = "Current"
 
         self.employees.append(employee)
@@ -21,12 +33,16 @@ class Class:
     def printClass(self):
         print(f"Class {self.classnumber}")
 
+        if (len(self.students) == 0) and (len(self.employees) == 0):
+            print("Class is empty.")
+            return
+
         print("Students:")
         for num in range(len(self.students)):
             if self.students[num].status == "Past":
                 continue
             else:
-                print(f"Student no. {num+1}: {self.students[num].firstname} {self.students[num].lastname}")
+                print(f"Student no. {num + 1}: {self.students[num].firstname} {self.students[num].lastname}")
 
         print("\n")
         print("Employees:")
@@ -34,4 +50,7 @@ class Class:
             if (self.employees[num].status == "Past") or (self.employees[num].status == "Dismissed"):
                 continue
             else:
-                print(f"Employee no. {num+1}: {self.employees[num].__class__.__name__} {self.employees[num].firstname} {self.employees[num].lastname}")
+                print(
+                    f"Employee no. {num + 1}: {self.employees[num].__class__.__name__} {self.employees[num].firstname} {self.employees[num].lastname}")
+
+        print("\n")
--- main.py
+++ main.py
@@ -1,42 +1,88 @@
 import Student
 import Class
 import Employees
-import Timetables
+import Events
 import FileManagement
 
+
 students = []
-empoyees = []
-
-s1 = Student.Student("Ben", "Bezzina", "137102L", "15/04/2002", "ben1024")
-s2 = Student.Student("Chris", "Azzopardi", "102088L", "05/02/2002", "ca2002")
-c1 = Class.Class(9, 2022)
-c1.addStudentToClass(s1)
-c1.addStudentToClass(s2)
+employees = []
+classes = []
 
 
-e1 = Employees.Playworker("Lilly", "Rosario", "349503M", "01/08/1972", "lillyros72", "Pending")
-e2 = Employees.ChildSupport("Ellen", "Foster", "827601M", "24/06/1985", "elfoster85", "Pending")
-c1.addEmployeeToClass(e1)
-c1.addEmployeeToClass(e2)
-
-c1.printClass()
-s1.editStatus("Past")
-e2.editStatus("Past")
-c1.printClass()
-
-#p1 = Payments.Payment(s1, 18, "June", "2022")
-#p1.paymentPaid()
-#p2 = Payments.Payment(s1, 18, "August", "2022")
-#p2.paymentPaid()
-#p1.printPayments()
-s1.newPayment(18, "June", "2022")
-s1.paymentPaid()
-s1.newPayment(18, "August", "2022")
-s1.paymentPaid()
-s1.studentPayments()
-s1.printCurrentPayment()
-
-t1 = Timetables.Timetable(9)
-t1.printTimetable()
+def createStudents():
+    print("Creating Students...")
+    students.append(Student.Student("Ben", "Bezzina", "137102L", "15/04/2002", "ben1024"))
+    students.append(Student.Student("Chris", "Azzopardi", "102088L", "05/02/2002", "ca2002"))
+    students.append(Student.Student("Joanna", "Smith", "05676L", "19/08/2002", "jojo1234"))
 
 
+def creatingEmployees():
+    print("Creating Employees...")
+    employees.append(Employees.Playworker("Lilly", "Rosario", "349503M", "01/08/1972", "lillyros72", "Pending"))
+    employees.append(Employees.ChildSupport("Ellen", "Foster", "827601M", "24/06/1985", "elfoster85", "Pending"))
+    employees.append(Employees.ITSupport("George", "Vella", "554961M", "20/10/1974", "g_Vella2010", "Pending"))
+
+
+def classOperations():
+    print("Creating Class...")
+    classes.append(Class.Class(9, 2022))
+
+    print("\n")
+    classes[0].printClass()
+    print("\n")
+
+    print("Adding Students to Class...")
+    classes[0].addStudentToClass(students[0])
+    classes[0].addStudentToClass(students[1])
+    classes[0].addStudentToClass(students[2])
+
+    print("Adding Employees to Class...")
+    classes[0].addEmployeeToClass(employees[0])
+    classes[0].addEmployeeToClass(employees[1])
+
+    print("\n")
+    classes[0].printClass()
+
+
+def pastStatus():
+    print("Making some students and employees past...")
+    students[0].editStatus("Past")
+    employees[0].editStatus("Past")
+
+    classes[0].printClass()
+
+
+def paymentOperations():
+    students[1].newPayment(18, "June", "2022")
+    students[1].paymentPaid()
+    students[1].newPayment(18, "August", "2022")
+
+    students[1].studentPayments()
+
+    students[1].printCurrentPayment()
+
+    print("\nPaying what needs to be paid...")
+    students[1].paymentPaid()
+
+    students[1].studentPayments()
+
+    students[1].printCurrentPayment()
+
+
+def timetablePrinting():
+    classes[0].timetable.printTimetable()
+
+
+def eventsOperations():
+
+    event1 = Events.Event("Cinema Day", "05/07/2022", "Saint Julian's, Malta", "The classes will be going to Eden Cinemas to watch the new Puss in Boots movie.", "Upcoming")
+
+
+createStudents()
+creatingEmployees()
+classOperations()
+pastStatus()
+paymentOperations()
+timetablePrinting()
+eventsOperations()
